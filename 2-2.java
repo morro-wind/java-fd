0a5e5da22c1947ebf47f1ad2b98e22515cfbdca2
morro-wind <specter21@sina.com>
2017-07-04 23:53:49 +0800

    add 2 java

---
+++ 2-2.java
@@ -0,0 +1,8 @@
+public class Puppy{
+    public Puppy(){
+    }
+
+    public Puppy(String name){
+        //  这个构造器仅有一个参数：name
+    }
+}
---
+++ 2-4.java
@@ -0,0 +1,6 @@
+/*  实例化对象  */
+ObjectReference = new Constructor();
+/*  访问其中的变量  */
+ObjectReference.variableName;
+/*  访问类中的方法  */
+ObjectReference.MethodName();
