d2994b3c53f355e364570e4120f901c9d3655e67
morro-wind <imorrowind@hotmail.com>
2017-07-04 18:28:33 +0800

    add 1

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    /*  这是一个java 程序
+     *  他将打印Hello Wold
+     *  这是一个多行注释的示例
+     */
+    public static void main (String []args) {
+        //  这是单行注释的示例
+        /*  这个也是单行注释的示例  */
+        System.out.println ("Hello World");
+    }
+}
