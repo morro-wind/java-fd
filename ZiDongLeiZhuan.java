4402f4c6c1f1a1a0b00812cd786d56f7c700d27f
morro-wind <specter21@sina.com>
2017-07-07 04:37:32 +0800

    update

---
+++ ZiDongLeiZhuan.java
@@ -0,0 +1,10 @@
+public class ZiDongLeiZhuan{
+    public static void main(String[] args) {
+        char c1='a';    //定义一个char 类型
+        int i1 = c1;    //cahr 自动类型转换为int
+        System.out.println("char 自动类型转换为int后的值等于" + i1);
+        char c2 = 'A';  //定义一个char 类型
+        int i2 = c2 + 1;    //char 类型和int 类型计算
+        System.out.println("char 类型和int 计算后的值等于" + i2);
+    }
+}
